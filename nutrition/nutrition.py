cb2785a25cb2d927c5f8b021222222a39bbac91d
HsupoLeng <hsupo.leng@gmail.com>
2018-06-02 16:25:44 +0800

    Fix cannot restart crawl bug; Use multiproc

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -1,12 +1,22 @@
+import multiprocessing as mp
 from .crawler import GetResultCrawler
 from .hackscrap.hackscrap.spiders.nutrition_spider import NutritionSpider
 
 
-def scrap_nutrition(food=""):
+def _crawl(queue, food):
     crawler = GetResultCrawler()
     scrapped_item = crawler.crawl({NutritionSpider: {"food": food}})
-    return scrapped_item[0]
+    queue.put(scrapped_item[0])
 
+
+def scrap_nutrition(food=""):
+    q = mp.Queue()
+    p = mp.Process(target=_crawl, args=(q, food))
+    p.start()
+    retval = q.get()
+    p.join()
+
+    return retval
 
 '''
 if __name__ == "__main__":
